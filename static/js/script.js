dad8cc60d7a0ee131ef8d82352717a55031a255c
unseeyou <unseeyoubestmod@gmail.com>
2025-03-04 10:37:27 +1100

    fix: layout of homepage

--- static/js/script.js
+++ static/js/script.js
@@ -16,3 +16,17 @@ function filterCancelledTable() {
         }
     }
 }
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+window.onscroll = function() {
+    var scrollButton = document.getElementById("scrollButton");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {  // use OR so that it works on Chrome, Firefox, and Safari
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+};
