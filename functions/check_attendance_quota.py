8b36d20473237fdce81d121a949f88463e43d05c
unseeyou <unseeyoubestmod@gmail.com>
2025-05-26 12:11:25 +1000

    feat: function to check for exempted dates

--- functions/check_attendance_quota.py
+++ functions/check_attendance_quota.py
@@ -1,5 +1,10 @@
 from constants import app
 
+# TODO: check for optional sessions -> missed do not count to total
+# PLAN:
+# 1. iterate through all students and see if they have met the quota
+# 2. if they have not met the quota, check if they meet the quota given the exempted dates in a second iteration with just those people
+
 
 def get_student_attendance_percentage_basic(student_id: str):
     cursor = app.database.get_cursor()
@@ -33,3 +38,11 @@ def get_exclusion_dates_for_student(student_id: str):
 
     exemptions = cursor.fetchall()
     return exemptions if len(exemptions) > 0 else ["no data"]
+
+
+def check_basic(students: list[str]):
+    need_more_check = []
+    for student in students:
+        if get_student_attendance_percentage_basic(student) < 0.85:
+            need_more_check.append(student)
+    return need_more_check
